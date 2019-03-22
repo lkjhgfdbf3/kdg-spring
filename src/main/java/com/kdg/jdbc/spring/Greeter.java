3132ad9766ec0eb94c537a2816e9350128d6e89b
김동규 <96kdg@naver.com>
2019-03-22 18:20:54 +0900

    올림

---
+++ src/main/java/com/kdg/jdbc/spring/Greeter.java
@@ -0,0 +1,12 @@
+package com.kdg.jdbc.spring;
+
+public class Greeter {
+	public Greeter() {
+		System.out.println("Greeter instance created.");
+	}
+	
+	public String greet() {
+		return "Hello, World!";
+	}
+
+}
