2c3db0fcfece0fb65e35617f80049ef7b554c041
lkjhgfdbf3 <sd0809765>
2019-04-05 14:32:56 +0900

    ...

---
+++ src/main/java/com/kdg/article/ArticleService.java
@@ -0,0 +1,73 @@
+package com.kdg.article;
+
+import java.util.List;
+
+/**
+ * articleDao를 사용해서 글 목록, 조회, 추가, 수정, 삭제를 한다.
+ * 
+ * @author Jacob
+ */
+public class ArticleService {
+
+	ArticleDao articleDao;
+
+	public void setArticleDao(ArticleDao articleDao) {
+		this.articleDao = articleDao;
+	}
+
+	/**
+	 * 글 목록
+	 */
+	public void listArticles() {
+		List<Article> articles = articleDao.listArticles();
+		System.out.println(articles);
+	}
+
+	/**
+	 * 글 조회
+	 */
+	public void getArticle() {
+		Article article = articleDao.getArticle("3");
+		System.out.println(article);
+	}
+
+	/**
+	 * 글 등록
+	 */
+	public void addArticle() {
+		Article article = new Article();
+		article.setTitle("야구는 흐름' 공식도 안통한 SK 홈런군단의 극적 부활");
+		article.setContent(
+				"2019 KBO리그 롯데 자이언츠와 SK 와이번스의 경기가 4일 오후 인천 SK 행복드림구장에서 열렸다. 7회말 2사 SK 이재원이 솔로포를 치고 들어오며 축하받고 있다.");
+		article.setUserId("1");
+		article.setName("김동규");
+		if (articleDao.addArticle(article) > 0)
+			System.out.println("글을 추가했습니다.");
+		else
+			System.out.println("글을 추가하지 못했습니다.");
+	}
+
+	/**
+	 * 글 수정
+	 */
+	public void updateArticle() {
+		Article article = new Article();
+		article.setArticleId("7");
+		article.setTitle("This is modified title.");
+		article.setContent("This is modified content");
+		if (articleDao.updateArticle(article) > 0)
+			System.out.println("글을 수정했습니다.");
+		else
+			System.out.println("글을 수정하지 못했습니다.");
+	}
+
+	/**
+	 * 글 삭제
+	 */
+	public void deleteArticle() {
+		if (articleDao.deleteArticle("8") > 0)
+			System.out.println("글을 삭제했습니다.");
+		else
+			System.out.println("글을 삭제하지 못했습니다.");
+	}
+}
---
+++ src/main/java/com/kdg/article/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring JDBC를 사용해서 Article 애플리케이션 구현
+ */
+package com.kdg.article;
